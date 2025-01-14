e4d7e48c8907bd8ef6ba42fcd8a71322977f3899
Jeffrey Huang <jeffdh5@gmail.com>
2025-01-14 14:21:46 -0600

    fix

--- idx-template.nix
+++ idx-template.nix
@@ -1,17 +1,9 @@
-# No user-configurable parameters
-{ pkgs, ... }: {
-  # Shell script that produces the final environment
+{ pkgs, ENV_VAR1 ? "default1", ENV_VAR2 ? "default2", ... }: {
   bootstrap = ''
-    # Copy the folder containing the `idx-template` files to the final
-    # project folder for the new workspace. ${./.} inserts the directory
-    # of the checked-out Git folder containing this template.
-    cp -rf ${./.} "$out"
-
-    # Set some permissions
-    chmod -R +w "$out"
-
-    # Remove the template files themselves and any connection to the template's
-    # Git repository
-    rm -rf "$out/.git" "$out/idx-template".{nix,json}
+    mkdir -p "$WS_NAME"
+    cp -r ${./.}/* "$WS_NAME"
+    echo "ENV_VAR1=${ENV_VAR1}" > "$WS_NAME/.env"
+    echo "ENV_VAR2=${ENV_VAR2}" >> "$WS_NAME/.env"
+    chmod +w "$WS_NAME/.env"
   '';
 }
